8f5508fae4b62019f10dc071ea84145196c2e270
Matt Bodman <mattbodman@gmail.com>
2018-05-10 11:19:12 +1000

    version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='vhxclient',
-      version='0.1',
+      version='0.2',
       description='A Python Client for the VHX API',
       url='https://github.com/mattbodman/vhxclient',
       author='Matt Bodman',
